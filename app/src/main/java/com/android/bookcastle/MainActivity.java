46d13cad7045cb950749f007a2927699c4819b41
mourabitiy <mourabitiyoussef@gmail.com>
2022-11-15 12:47:05 +0100

    HomeScreen - V1

--- app/src/main/java/com/android/bookcastle/MainActivity.java
+++ app/src/main/java/com/android/bookcastle/MainActivity.java
@@ -1,44 +1,93 @@
 package com.android.bookcastle;
 
-import static android.content.ContentValues.TAG;
-import static android.content.Context.*;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.ConnectivityManager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.android.bookcastle.utils.BooksDao;
 import com.android.bookcastle.utils.NetworkChangeListener;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 
-import java.util.concurrent.ExecutionException;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
     NetworkChangeListener networkChangeListener = new NetworkChangeListener();
+    ImageView mDogImageView;
+    Button nextDogButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        mDogImageView = findViewById(R.id.dogImageView);
+//        nextDogButton = findViewById(R.id.nextDogButton);
+
+       // nextDogButton.setOnClickListener(View -> loadDogImage());
+
+        // image of a dog will be loaded once at the start of the app
+        //loadDogImage();
     }
 
-    @Override
-    protected void onStart() {
-        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
-        registerReceiver(networkChangeListener,intentFilter);
-        super.onStart();
-    }
+//    private void loadDogImage() {
+//
+//        // getting a new volley request queue for making new requests
+//        RequestQueue volleyQueue = Volley.newRequestQueue(MainActivity.this);
+//        // url of the api through which we get random dog images
+//        String url = "https://dog.ceo/api/breeds/image/random";
+//
+//        // since the response we get from the api is in JSON, we
+//        // need to use `JsonObjectRequest` for parsing the
+//        // request response
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+//                // we are using GET HTTP request method
+//                Request.Method.GET,
+//                // url we want to send the HTTP request to
+//                url,
+//                // this parameter is used to send a JSON object to the
+//                // server, since this is not required in our case,
+//                // we are keeping it `null`
+//                null,
+//
+//                // lambda function for handling the case
+//                // when the HTTP request succeeds
+//                (Response.Listener<JSONObject>) response -> {
+//                    // get the image url from the JSON object
+//                    String dogImageUrl;
+//                    try {
+//                        dogImageUrl = response.getString("message");
+//                        // load the image into the ImageView using Glide.
+//                        Glide.with(MainActivity.this).load(dogImageUrl).into(mDogImageView);
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
+//                },
+//
+//                // lambda function for handling the case
+//                // when the HTTP request fails
+//                (Response.ErrorListener) error -> {
+//                    // make a Toast telling the user
+//                    // that something went wrong
+//                    Toast.makeText(MainActivity.this, "Some error occurred! Cannot fetch dog image", Toast.LENGTH_LONG).show();
+//                    // log the error message in the error stream
+//                    Log.e("MainActivity", "loadDogImage error: ${error.localizedMessage}");
+//                }
+//        );
+//
+//        // add the json request object created above
+//        // to the Volley request queue
+//        volleyQueue.add(jsonObjectRequest);
+//    }
 
-    @Override
-    protected void onStop() {
-        unregisterReceiver(networkChangeListener);
-        super.onStop();
-    }
+
 }
