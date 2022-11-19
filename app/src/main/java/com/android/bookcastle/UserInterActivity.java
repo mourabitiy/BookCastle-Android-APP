8ffcc6ab06fffaf8a5510699b86a803d40f445a5
Samia Daize <daizesamia@gmail.com>
2022-11-19 23:31:58 +0100

    user Interface

---
+++ app/src/main/java/com/android/bookcastle/UserInterActivity.java
@@ -0,0 +1,14 @@
+package com.android.bookcastle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserInterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_inter);
+    }
+}
